dd5f119001d232366c447674f541ff962cdc166b
wchen <wchen@student.42tokyo.jp>
2023-05-18 23:14:05 +0900

    submit 20230518

--- objs/srcs/do_action.d
+++
@@ -1,6 +0,0 @@
-objs/./srcs/do_action.o: srcs/do_action.c includes/philosophers.h \
-  includes/../utils/includes/libutils.h
-
-includes/philosophers.h:
-
-includes/../utils/includes/libutils.h:
--- objs/srcs/main.d
+++
@@ -1,6 +0,0 @@
-objs/./srcs/main.o: srcs/main.c includes/philosophers.h \
-  includes/../utils/includes/libutils.h
-
-includes/philosophers.h:
-
-includes/../utils/includes/libutils.h:

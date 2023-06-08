92fdd299222adf53f7e169b4e64358b653046713
wchen <wchen@student.42tokyo.jp>
2023-06-08 20:38:23 +0900

    submit 20230609

--- philo/objs/srcs/thread_monitor.d
+++
@@ -1,6 +0,0 @@
-objs/./srcs/thread_monitor.o: srcs/thread_monitor.c \
-  includes/philosophers.h includes/../utils/includes/libutils.h
-
-includes/philosophers.h:
-
-includes/../utils/includes/libutils.h:

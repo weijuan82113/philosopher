bce303b25a58b457155a8a53c08d600db819dae9
wchen <wchen@student.42tokyo.jp>
2023-06-25 13:23:43 +0900

    submit 20230625-3

---
+++ philo/objs/srcs/judge_finish.d
@@ -0,0 +1,6 @@
+objs/./srcs/judge_finish.o: srcs/judge_finish.c includes/philosophers.h \
+  includes/../utils/includes/libutils.h
+
+includes/philosophers.h:
+
+includes/../utils/includes/libutils.h:
